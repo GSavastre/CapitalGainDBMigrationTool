0b9d008d5b12c2a97ce0e7e0274e6ec25b2d5a19
Gabriele <savastrecosmingabriele@gmail.com>
2020-09-18 17:02:40 +0200

    Table and table attribute

---
+++ CapitalGainDBMigrationTool/TableItems/Table.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CapitalGainDBMigrationTool.TableItems {
+    class Table {
+
+        public string id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+
+        List<TableAttribute> attributes = new List<TableAttribute>();
+
+        public Table() { }
+    }
+}
